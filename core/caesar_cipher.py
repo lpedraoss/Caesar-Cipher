89e22d1c4c72d08802105e6fba7f4e27b65abf4d
lpedraoss <alfonso.suarez.dev@gmail.com>
2024-08-05 16:39:08 -0500

    added: documentation for caesar cipher

--- core/caesar_cipher.py
+++ core/caesar_cipher.py
@@ -1,5 +1,35 @@
 class CaesarCipher:
+    """
+    Class representing a Caesar Cipher encryption and decryption algorithm.
+    Args:
+        idioma (str, optional): The language to use for the cipher. Defaults to 'es'.
+        especial (bool, optional): Whether to include special characters and numbers in the cipher. Defaults to False.
+    Attributes:
+        alphabet (dict): The alphabet used for the cipher.
+        alphabet_en (dict): The English alphabet.
+        alphabet_es (dict): The Spanish alphabet.
+        especial_en (dict): Special characters and numbers in English.
+        especial_es (dict): Special characters and numbers in Spanish.
+        numbers_en (dict): Numbers in English.
+        numbers_es (dict): Numbers in Spanish.
+        modulus (int): The modulus used for the cipher.
+        indices (dict): A dictionary mapping indices to characters in the alphabet.
+    Methods:
+        set_alphabet(idioma, especial): Sets the alphabet based on the language and special character options.
+        encrypt(text, shift): Encrypts the given text using the specified shift value.
+        decrypt(encrypted_text, shift): Decrypts the given encrypted text using the specified shift value.
+    """
     def __init__(self, idioma='es', especial=False):
+        """
+        Initializes the CaesarCipher object.
+
+        Parameters:
+        - idioma (str): The language to use for the cipher. Default is 'es' (Spanish).
+        - especial (bool): Whether to include special characters in the cipher. Default is False.
+
+        Returns:
+        None
+        """
         self.alphabet = None
         self.alphabet_en = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
         self.alphabet_es = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'ñ': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
@@ -25,6 +55,16 @@ class CaesarCipher:
         self.set_alphabet(idioma, especial)
 
     def set_alphabet(self, idioma, especial):
+        """
+        Sets the alphabet based on the specified language and special character flag.
+
+        Parameters:
+            idioma (str): The language to set the alphabet for. Valid values are 'en' for English and 'es' for Spanish.
+            especial (bool): Flag indicating whether to include special characters in the alphabet.
+
+        Returns:
+            None
+        """
         if idioma == 'en':
             self.alphabet = self.alphabet_en
             if especial:
@@ -44,6 +84,19 @@ class CaesarCipher:
         self.indices = {v: k for k, v in self.alphabet.items()}
 
     def encrypt(self, text, shift):
+        """
+        Encrypts the given text using the Caesar cipher algorithm.
+        Parameters:
+        - text (str): The text to be encrypted.
+        - shift (int): The number of positions to shift each character.
+        Returns:
+        - encrypted_text (str): The encrypted text.
+        Example:
+        cipher = CaesarCipher()
+        encrypted_text = cipher.encrypt("Hello, World!", 3)
+        print(encrypted_text)
+        # Output: "Khoor, Zruog!"
+        """
         encrypted_text = ''
         for char in text:
             if char in self.alphabet:
@@ -57,6 +110,17 @@ class CaesarCipher:
         return encrypted_text
 
     def decrypt(self, encrypted_text, shift):
+        """
+        Decrypts the given encrypted text using the Caesar cipher algorithm.
+
+        Parameters:
+        - encrypted_text (str): The text to be decrypted.
+        - shift (int): The number of positions to shift each character.
+
+        Returns:
+        - decrypted_text (str): The decrypted text.
+
+        """
         decrypted_text = ''
         for char in encrypted_text:
             if char in self.alphabet:
